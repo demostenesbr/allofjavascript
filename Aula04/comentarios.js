3a6f6a61add29deecebacf519ec607caeaf57d27
Demóstenes Muniz Brito <demostenes@demostenes.com.br>
2025-01-13 23:03:18 -0300

    Feat: Aula 04 e comentários

---
+++ Aula04/comentarios.js
@@ -0,0 +1,14 @@
+// Escrever um comentário
+console.log('Olá mundo!');
+console.log('Linha 1');
+console.log('Linha 2');
+// Comentário
+console.log('Linha 3');
+console.log('Linha 4');
+console.log('Linha 5');
+// Aqui tem outro comentário
+console.log('Linha 6');
+
+/*
+console.log('Linha 7');
+*/
