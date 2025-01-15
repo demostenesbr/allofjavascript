c3d7cac38be5821f07e0f1231244550fc875820d
Demóstenes Muniz Brito <demostenes@demostenes.com.br>
2025-01-15 00:21:16 -0300

    Feat: aula 06 e let

---
+++ Aula06/index.js
@@ -0,0 +1,23 @@
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar com o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// É permitido utilizar camelCase
+// Case-sensitive faz diferença
+// Não podemos declarar mais de uma vez variáveis com let
+// Não utilize var, utilize let
+
+let nomeCompletoDoCliente = 'João';
+nomeCompletoDoCliente = 'Maria';
+
+console.log(nomeCompletoDoCliente);
+
+// Não pode começar o nome de uma variável com um número
+
+/*
+let nome;
+nome = 'Qualquer valor';
+console.log(nome);
+nome = 'Otávio';
+console.log(nome);
+*/
