6ff61277562ae79ea1465f209a7a5d3e3af0c09a
Demóstenes Muniz Brito <demostenes@demostenes.com.br>
2025-01-22 23:24:58 -0300

    Feat: aula 09 - Boolean

--- Aula10/index.js
+++ Aula10/index.js
@@ -1,1 +1,20 @@
 // Tipos de dados primitivos
+// String, number, underfined, null, boolean, symbol
+
+const nome = 'Demóstenes'; // string
+const nome1 = 'Demóstenes'; // string
+const nome2 = `Demóstenes`; // string
+const num1 = 10; // number
+const num2 = 10.52; // number
+let nomeAluno; // underfined -> não aponta para local nenhum na memória
+const sobrenomeAluno = null; // Nulo -> não aponta para local nenhum na memória
+const aprovado = false; // Boolean = true, false (lógico)
+
+// console.log(typeof sobrenomeAluno, sobrenomeAluno);
+
+const a = [1, 2];
+const b = a;
+console.log(a, b);
+
+b.push(3);
+console.log(a, b);
