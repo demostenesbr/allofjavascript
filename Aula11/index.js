24322b674bf35d36736be4804c81ecd7d8b3aaa0
Demóstenes Muniz Brito <demostenes@demostenes.com.br>
2025-01-22 23:38:39 -0300

    Feat: Aula 11 - NaN

---
+++ Aula11/index.js
@@ -0,0 +1,43 @@
+/*
+    Aritiméticos
+    + Adição / Concatenação
+    - Subtração
+    * Multiplicação
+    / Divisão
+    ** Potenciação
+    % Resto da divisão
+*/
+
+const num1 = 5;
+const num2 = 2;
+const num3 = 10;
+console.log((num1 + num2) * num2);
+
+let contador = 1;
+contador++;
+contador++;
+contador++;
+contador++;
+
+console.log(contador++);
+
+
+const passo = 50;
+let contator = 0;
+
+contador = contator + passo;
+console.log(contador);
+
+contador = contator - passo;
+console.log(contador);
+
+contador = contator * passo;
+console.log(contador);44
+
+// NaN - Not a Number - parseInt (inteiro), parseFloat(decimais), Number
+const num4 = 10;
+const num5 = Number('5.2'); // Remove decimal case - parseInt - parseFloat - Number
+console.log(num4 * num5);
+console.log(typeof num5);
+
+
